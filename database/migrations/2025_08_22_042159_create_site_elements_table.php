97a86bc1b2ffda44d5d6a992090c45e0f1e8329e
poseymec <poseymec22@gmail.com>
2025-08-22 06:43:49 +0100

    deuxieme commit du site

---
+++ database/migrations/2025_08_22_042159_create_site_elements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('site_elements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('type'); //  'header', 'footer', 'sidebar', etc.
+            $table->string('content'); // contenu html ,texte ou url des images et logo
+            $table->string('status')->default('active'); // e.g., 'active', 'inactive'
+            $table->foreignId('produit_id')->nullable()->constrained('produits')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('site_elements');
+    }
+};
